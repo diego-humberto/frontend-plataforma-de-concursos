644500d2ba8c6394c9ccb089b7b50fde22b225cc
Diego Silva <diegohumberto5@gmail.com>
2025-05-15 23:33:23 -0300

    fix: atualizar citação no rodapé para uma nova frase inspiradora

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,9 +5,8 @@ export default function Footer({}: Props) {
     <footer className=" py-6">
       <div className="container mx-auto text-center">
         <blockquote className="text-sm italic">
-          "Se você deseja fazer uma torta de maçã do zero, você deve, primeiro,
-          criar o universo."
-          <cite className="block mt-2 font-medium">Carl Sagan</cite>
+          "Se está difícil, é sinal de que você está avançando!"
+          <cite className="block mt-2 font-medium">Alexandre Soares</cite>
         </blockquote>
       </div>
     </footer>
